a6efac2c1d9d77893578786137f3e8b67a1c270d
code-playmaker <s.ueno1229@gmail.com>
2020-09-20 11:42:09 +0900

    [Edit] migration_file (assginment)

--- db/migrate/20200920023547_create_assignments.rb
+++ db/migrate/20200920023547_create_assignments.rb
@@ -1,7 +1,10 @@
 class CreateAssignments < ActiveRecord::Migration[5.2]
   def change
     create_table :assignments do |t|
-
+      t.string :department,   null: false
+      t.string :division,     null: false
+      t.string :charge,       null: false
+      t.references :user,     foreign_key: true, null:false
       t.timestamps
     end
   end
