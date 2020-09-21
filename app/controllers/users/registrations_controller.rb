80affd027ef2c7827ff16eac8748116abb532a02
code-playmaker <s.ueno1229@gmail.com>
2020-09-21 17:15:08 +0900

    [Edit] registration_controller.rb(ユーザー登録のロジック作成)

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,62 +1,42 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    @user = User.new
+  end
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    @user = User.new(user_params)
+    unless @user.valid?
+      flash.now[alert] = @user.errors.full_messages
+      render :new and return
+    end
+    session["devise.regist_data"] = {user: @user.attributes}
+    session["devise.regist_data"] = user_params
+    @assignment = @user.assignments.build
+    render :new_assignment and return
+  end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def create_assignment
+    @user = User.new(session["devise.regist_data"])
+    @assignment = Assignment.new(assignment_params)
+    unless @assignment.valid?
+      flash.now[alert] = @assignment.errors.full_messages
+      render :new and return
+    end
+    @user.assignment.build(@assignment.attributes)
+    @user.save!
+    sign_in(:user, @user)
+    redirect_to root_path
+  end
 
-  # PUT /resource
-  # def update
-  #   super
-  # end
+  private
 
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
+  def user_params
+    params.require(:user).permit(:name, :email, :number, :password, :password_confirmation)
+  end
 
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
+  def assignment_params
+    params.require(:assignment).permit(:department, :division, :charge)
+  end
 
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 end
