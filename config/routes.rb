3d6ca8fc743e344703baf09482dd5d24b2823553
code-playmaker <s.ueno1229@gmail.com>
2020-09-26 15:38:22 +0900

    [Edit]routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
     post 'create_assignment', to: 'users/registrations#create_assignment'
   end
   root 'tops#index'
+  resources :messages, only: [:index, :new, :create]
 end
