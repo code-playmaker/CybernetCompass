192f61aa7cfcecc19b7ec32c506de6012cd92ddf
code-playmaker <s.ueno1229@gmail.com>
2020-09-27 15:14:46 +0900

    [Edit] controller(message)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,5 +3,22 @@ class MessagesController < ApplicationController
   end
 
   def new
+    @message = Message.new
+  end
+
+  def create
+    message_params[:upload_file] = upload_file.read
+    upload_file = message_params[:upload_file]
+    if upload_file.save
+      message_params[:upload_file_name] = upload_file.original_filename
+    end
+    @message = Message.new(message_params)
+    redirect_to root_path
+  end
+
+  private
+  
+  def message_params
+    params.require(:message).permit(:title, :content, :importancy, :emergency, :upload_file_name, :upload_file).merge(user_id: current_user.id)
   end
 end
