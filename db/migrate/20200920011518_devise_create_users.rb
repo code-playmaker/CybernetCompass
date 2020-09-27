3320e10fa571d8f052bdb488ef2818d41c043c64
code-playmaker <s.ueno1229@gmail.com>
2020-09-27 16:07:47 +0900

    [Edit] migration_file (users)

--- db/migrate/20200920011518_devise_create_users.rb
+++ db/migrate/20200920011518_devise_create_users.rb
@@ -8,6 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :name,                   null: false
       t.string :number,                 null: false, default: ""
       t.string :encrypted_password,     null: false, default: ""
+      t.integer :assignment_id,         null: false
 
       ## Recoverable
       t.string   :reset_password_token
