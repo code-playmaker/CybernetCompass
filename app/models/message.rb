0e44d898fb529b203723da0294c61beab2223e54
code-playmaker <s.ueno1229@gmail.com>
2020-09-27 15:14:25 +0900

    [Edit] model (message)

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,10 @@
 class Message < ApplicationRecord
-  belongs_to: user
-  validates :title, :content, presence: true
+  belongs_to :user
+  validates :title, :content, :importancy, :emergency, presence: true
+  enum importancy:{
+    高:1,中:2,低:3
+  }, _prefix: :importancy
+  enum emergency:{
+    高:1,中:2,低:3
+  }, _prefix: :emergency
 end
