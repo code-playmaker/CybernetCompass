3af8884ac622372c8a3c226a613d2447dc5daa43
code-playmaker <s.ueno1229@gmail.com>
2020-10-03 10:55:07 +0900

    [Edit] model (user)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   belongs_to :assignment, optional: true
   has_many :messages
+  has_many :circulations
+  has_many :notices
   validates :name, presence: true
-  validates :email, uniqueness: true
   validates :number, presence: true, uniqueness: true
   validates :password, confirmation: true
 
@@ -15,6 +16,10 @@ class User < ApplicationRecord
   end
 
   def email_changed?
+    false
+  end
+
+  def will_save_change_to_email?
     false
   end
 
