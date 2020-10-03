4c023cc0b97d4ba03c5a7f48c5d63f6b11ef20ff
code-playmaker <s.ueno1229@gmail.com>
2020-10-03 10:57:25 +0900

    [Edit] model (assignment)

--- db/migrate/20200920023547_create_assignments.rb
+++ db/migrate/20200920023547_create_assignments.rb
@@ -4,8 +4,7 @@ class CreateAssignments < ActiveRecord::Migration[5.2]
       t.string :department,   null: false
       t.string :division,     null: false
       t.string :charge,       null: false
-      t.integer :user_id,     null: false
-      t.references :users,     foreign_key: true, index: true
+      t.references :user,     foreign_key: true, index: true
       t.timestamps
     end
   end
